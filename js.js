bf1fbfc8289d8408b061c1bcc13adfad7ffe2659
SofiLeonar <143642462+SofiLeonar@users.noreply.github.com>
2025-03-11 14:00:46 -0300

    Feat: efecto del banner

--- js.js
+++ js.js
@@ -1,3 +1,30 @@
+// Banner
+const bannerContainer = document.querySelector('.banner-container');
+const codeBanner = document.querySelector('.code');
+const arteBanner = document.querySelector('.arte');
+
+bannerContainer.addEventListener('mousemove', (e) => {
+    const containerWidth = bannerContainer.offsetWidth;
+    const mouseX = e.clientX;
+
+    if (mouseX < containerWidth / 2) {
+        codeBanner.classList.add('expanded');
+        codeBanner.classList.remove('hidden');
+        arteBanner.classList.remove('expanded');
+        arteBanner.classList.add('hidden');
+    } else {
+        arteBanner.classList.add('expanded');
+        arteBanner.classList.remove('hidden');
+        codeBanner.classList.remove('expanded');
+        codeBanner.classList.add('hidden');
+    }
+});
+
+bannerContainer.addEventListener('mouseleave', () => {
+    codeBanner.classList.remove('expanded', 'hidden');
+    arteBanner.classList.remove('expanded', 'hidden');
+});
+
 // Linterna
 
 document.addEventListener("DOMContentLoaded", () => {
